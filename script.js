4011e60a1ea3fd2f5ea403a23a69bbeaa8ab03e4
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-06-24 21:11:38 +0700

    validation character

--- script.js
+++ script.js
@@ -5,20 +5,39 @@ document.getElementById("submit").addEventListener("click", function () {
   const password = document.getElementById("password").value.trim();
 
   const valid = [];
-
   for (let i = 1; i <= 4; i++) {
     valid[i] = document.getElementById(`valid${i}`);
   }
 
-  if (firstName === "") valid[1].innerText = "First name cannot be blank";
-  else valid[1].innerText = "";
+  // Validasi First Name
+  if (firstName === "") {
+    valid[1].innerText = "First name cannot be blank";
+  } else {
+    valid[1].innerText = "";
+  }
 
-  if (lastName === "") valid[2].innerText = "Last name cannot be blank";
-  else valid[2].innerText = "";
+  // Validasi Last Name
+  if (lastName === "") {
+    valid[2].innerText = "Last name cannot be blank";
+  } else {
+    valid[2].innerText = "";
+  }
 
-  if (email === "") valid[3].innerText = "Email cannot be blank";
-  else valid[3].innerText = "";
+  // Validasi Email
+  if (email === "") {
+    valid[3].innerText = "Email cannot be blank";
+  } else if (!email.includes("@")) {
+    valid[3].innerText = "Email must contain '@'";
+  } else {
+    valid[3].innerText = "";
+  }
 
-  if (password === "") valid[4].innerText = "Password cannot be blank";
-  else valid[4].innerText = "";
+  // Validasi Password
+  if (password === "") {
+    valid[4].innerText = "Password cannot be blank";
+  } else if (password.length <= 6) {
+    valid[4].innerText="Password must be at least 6 characters";
+  } else {
+    valid[4].innerText = "";
+  }
 });
